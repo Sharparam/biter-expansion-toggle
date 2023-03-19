f4047826bb1684a64bbc08194eddc33e8ac74e5a
Adam Hellberg <sharparam@sharparam.com>
2023-03-19 18:10:11 +0100

    Remove usage of _G

--- src/control.lua
+++ src/control.lua
@@ -1,15 +1,15 @@
 local function enable_expansion(player)
-  _G.game.map_settings.enemy_expansion.enabled = true
+  game.map_settings.enemy_expansion.enabled = true
   player.print({"biter-expansion-toggle.expansion-enabled"})
 end
 
 local function disable_expansion(player)
-  _G.game.map_settings.enemy_expansion.enabled = false
+  game.map_settings.enemy_expansion.enabled = false
   player.print({"biter-expansion-toggle.expansion-disabled"})
 end
 
 local function toggle_expansion(player)
-  local enabled = _G.game.map_settings.enemy_expansion.enabled
+  local enabled = game.map_settings.enemy_expansion.enabled
   if enabled then
     disable_expansion(player)
   else
@@ -18,6 +18,6 @@ local function toggle_expansion(player)
 end
 
 _G.commands.add_command("toggle_expansion", {"biter-expansion-toggle.toggle-cmd-help"}, function(command)
-  local player = _G.game.get_player(command.player_index)
+  local player = game.get_player(command.player_index)
   toggle_expansion(player)
 end)
