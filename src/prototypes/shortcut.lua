aaf2b1a91283ecb86507b232f8a216184c9fc4b2
Adam Hellberg <sharparam@sharparam.com>
2023-03-20 23:40:12 +0100

    Add missing small and 'disabled' shortcut icons

--- src/prototypes/shortcut.lua
+++ src/prototypes/shortcut.lua
@@ -11,6 +11,21 @@ data:extend {
       filename = "__biter-expansion-toggle__/graphics/icons/shortcut_x32.png",
       size = 32,
       flags = {"gui-icon"}
+    },
+    disabled_icon = {
+      filename = "__biter-expansion-toggle__/graphics/icons/shortcut_x32_white.png",
+      size = 32,
+      flags = {"gui-icon"}
+    },
+    small_icon = {
+      filename = "__biter-expansion-toggle__/graphics/icons/shortcut_x24.png",
+      size = 24,
+      flags = {"gui-icon"}
+    },
+    disabled_small_icon = {
+      filename = "__biter-expansion-toggle__/graphics/icons/shortcut_x24_white.png",
+      size = 24,
+      flags = {"gui-icon"}
     }
   }
 }
